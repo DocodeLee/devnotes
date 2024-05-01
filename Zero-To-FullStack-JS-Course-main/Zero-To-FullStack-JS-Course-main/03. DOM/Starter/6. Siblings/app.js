8c6fbc822b564ce0b06d591560ff78fa504d13dd
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-05-01 23:41:31 +0900

    sibling

---
+++ Zero-To-FullStack-JS-Course-main/Zero-To-FullStack-JS-Course-main/03. DOM/Starter/6. Siblings/app.js
@@ -0,0 +1,14 @@
+// let firstLi = document.querySelector("li");
+// console.log(firstLi.parentElement); //<ul>
+// console.log(firstLi.parentElement.parentElement); //body
+
+// let ul = document.querySelector("ul");
+// console.log(ul.children);
+// console.log(ul.children[0]);
+// console.log((ul.children[0].innerText = "one"));
+// console.log(firstLi.nextElementSibling);
+
+let fourth = document.querySelector(".fourth");
+console.log(fourth.textContent);
+
+// previousElementSibling, nextElementSibling;
